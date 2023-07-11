9066e1ade57c3ac96695a03b69fa731da75988a1
Lechosław Parol <lechu0715@gmail.com>
2023-07-11 10:08:19 +0200

    Add JobOfferController

---
+++ JobBoard/Controllers/JobOfferController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobBoard.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobOfferController : ControllerBase
+    {
+    }
+}
